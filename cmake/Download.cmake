d28a092da01f19908bb05cf37afe2ddf110fcf62
Eduardo Moisa <emoisa@jcm-tech.local>
2025-07-22 14:12:05 +0200

    build: reorgamize cmake files into a better structure

--- cmake/Download.cmake
+++ cmake/Download.cmake
@@ -20,15 +20,16 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Function occupied to download the CEF library
 function(DownloadCEF platform version download_dir)
-    # specify the binary distribution type and download directory
+    # Specify the binary distribution type and download directory
     set(CEF_DISTRIBUTION "cef_binary_${version}_${platform}")
     set(CEF_DOWNLOAD_DIR "${download_dir}")
 
-    # the location where we expect the extracted binary distribution
+    # The location where we expect the extracted binary distribution
     set(CEF_ROOT "${CEF_DOWNLOAD_DIR}/${CEF_DISTRIBUTION}" CACHE INTERNAL "CEF_ROOT")
 
-    # download and/or extract the binary distribution if necessary
+    # Download and/or extract the binary distribution if necessary
     if(NOT IS_DIRECTORY "${CEF_ROOT}")
         set(CEF_DOWNLOAD_FILENAME "${CEF_DISTRIBUTION}.tar.bz2")
         set(CEF_DOWNLOAD_PATH "${CEF_DOWNLOAD_DIR}/${CEF_DOWNLOAD_FILENAME}")
@@ -37,14 +38,14 @@ function(DownloadCEF platform version download_dir)
             set(CEF_DOWNLOAD_URL "https://cef-builds.spotifycdn.com/${CEF_DOWNLOAD_FILENAME}")
             string(REPLACE "+" "%2B" CEF_DOWNLOAD_URL_ESCAPED ${CEF_DOWNLOAD_URL})
 
-            # download the SHA1 hash for the binary distribution
-            message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}.sha1 from ${CEF_DOWNLOAD_URL_ESCAPED}...")
+            # Download the SHA1 hash for the binary distribution
+            message(STATUS "Download: ${CEF_DOWNLOAD_PATH}.sha1 from ${CEF_DOWNLOAD_URL_ESCAPED}")
 
             file(DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}.sha1" "${CEF_DOWNLOAD_PATH}.sha1")
             file(READ "${CEF_DOWNLOAD_PATH}.sha1" CEF_SHA1)
 
-            # download the binary distribution and verify the hash
-            message(STATUS "Downloading ${CEF_DOWNLOAD_PATH}...")
+            # Download the binary distribution and verify the hash
+            message(STATUS "Download: ${CEF_DOWNLOAD_PATH}")
 
             file(
                 DOWNLOAD "${CEF_DOWNLOAD_URL_ESCAPED}" "${CEF_DOWNLOAD_PATH}"
@@ -53,8 +54,8 @@ function(DownloadCEF platform version download_dir)
             )
         endif()
 
-        # extract the binary distribution
-        message(STATUS "Extracting ${CEF_DOWNLOAD_PATH}...")
+        # Extract the binary distribution
+        message(STATUS "Download: Extracting ${CEF_DOWNLOAD_PATH}...")
         execute_process(
             COMMAND ${CMAKE_COMMAND} -E tar xzf "${CEF_DOWNLOAD_DIR}/${CEF_DOWNLOAD_FILENAME}"
             WORKING_DIRECTORY ${CEF_DOWNLOAD_DIR}
---
+++ tests/main.py
@@ -0,0 +1,3 @@
+import cefpulse
+
+cefpulse.Initialize()
